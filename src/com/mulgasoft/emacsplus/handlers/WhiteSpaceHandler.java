894655f0959791610d3bb014ab2dc0800cca360f
Kevin Fowler <kfowler@connectifier.com>
2017-04-12 11:52:47 -0700

    Fix uninitialized variable issue.

--- src/com/mulgasoft/emacsplus/handlers/WhiteSpaceHandler.java
+++ src/com/mulgasoft/emacsplus/handlers/WhiteSpaceHandler.java
@@ -1,6 +1,6 @@
-// 
+//
 // Decompiled by Procyon v0.5.30
-// 
+//
 
 package com.mulgasoft.emacsplus.handlers;
 
@@ -21,7 +21,7 @@ public class WhiteSpaceHandler extends EmacsPlusWriteHandler
         }
         return this.transformSpace(editor, caret.getOffset(), dataContext, replace, ignoreCR);
     }
-    
+
     protected int transformSpace(@NotNull final Editor editor, final int offset, final DataContext dataContext, String replace, final boolean ignoreCR) {
         if (editor == null) {
             throw new IllegalArgumentException(String.format("Argument for @NotNull parameter '%s' of %s.%s must not be null", "editor", "com/mulgasoft/emacsplus/handlers/WhiteSpaceHandler", "transformSpace"));
@@ -36,20 +36,21 @@ public class WhiteSpaceHandler extends EmacsPlusWriteHandler
         document.replaceString(offset - left, offset + right, (CharSequence)replace);
         return offset - left + replace.length();
     }
-    
+
     protected boolean isBlankLine(final Document document, final int line) {
         final int offset = document.getLineStartOffset(line);
         final int lastOff = document.getTextLength();
         return this.countWS(document, lastOff, offset, 1, false) == document.getLineEndOffset(line) - offset;
     }
-    
+
     private int countWS(final Document document, final int lastOff, final int offset, final int dir, final boolean ignoreCR) {
         final CharSequence seq = document.getCharsSequence();
         final char eol = '\n';
         final char cr = '\r';
         int lineOff = offset;
         char c;
-        for (int off = offset; -1 < off && off < lastOff && (c = seq.charAt(off)) <= ' '; off += dir) {
+        int off = offset;
+        for (; -1 < off && off < lastOff && (c = seq.charAt(off)) <= ' '; off += dir) {
             if (c == eol || c == cr) {
                 if (!ignoreCR) {
                     break;
@@ -57,7 +58,6 @@ public class WhiteSpaceHandler extends EmacsPlusWriteHandler
                 lineOff = off + dir;
             }
         }
-        int off;
         return Math.abs(offset - (ignoreCR ? lineOff : off));
     }
 }
