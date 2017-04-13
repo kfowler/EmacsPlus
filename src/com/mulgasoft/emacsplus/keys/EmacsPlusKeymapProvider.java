a698554e4f7e2ab401cbed2808dcde5b1e500b1e
Kevin Fowler <kfowler@connectifier.com>
2017-04-12 18:01:58 -0700

    Add de,es,fr keymaps.

--- src/com/mulgasoft/emacsplus/keys/EmacsPlusKeymapProvider.java
+++ src/com/mulgasoft/emacsplus/keys/EmacsPlusKeymapProvider.java
@@ -1,21 +1,29 @@
 package com.mulgasoft.emacsplus.keys;
 
 import com.google.common.collect.ImmutableList;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.keymap.impl.BundledKeymapProvider;
-import com.intellij.util.io.URLUtil;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.List;
 import java.util.function.Function;
 import org.jetbrains.annotations.NotNull;
 
-
 public class EmacsPlusKeymapProvider implements BundledKeymapProvider {
-  ImmutableList<String> keymapFileNames = ImmutableList.of(
+
+  private static final Logger LOG = Logger.getInstance(EmacsPlusKeymapProvider.class);
+
+  private final ImmutableList<String> keymapFileNames = ImmutableList.of(
       "Keymap_EmacsPlus.xml",
-      "Keymap_EmacsPlusMac.xml"
+      "Keymap_EmacsPlusMac.xml",
+      "de/Keymap_EmacsPlus.xml",
+      "de/Keymap_EmacsPlusMac.xml",
+      "es/Keymap_EmacsPlus.xml",
+      "es/Keymap_EmacsPlusMac.xml",
+      "fr/Keymap_EmacsPlus.xml",
+      "fr/Keymap_EmacsPlusMac.xml"
   );
+
   @NotNull
   @Override
   public List<String> getKeymapFileNames() {
@@ -27,7 +35,6 @@ public class EmacsPlusKeymapProvider implements BundledKeymapProvider {
     /*
      * Hack to work around: https://github.com/JetBrains/intellij-community/pull/493
      */
-
     try (InputStream stream = this.getClass().getResourceAsStream(key)) {
       return consumer.apply(stream);
     } catch (Exception e) {
