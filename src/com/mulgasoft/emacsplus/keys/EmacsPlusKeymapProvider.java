d1ebf21c217346ba58522a06aa441b755644b595
Kevin Fowler <kfowler@connectifier.com>
2017-04-13 12:51:39 -0700

    ignore de, es, fr keymaps for now

--- src/com/mulgasoft/emacsplus/keys/EmacsPlusKeymapProvider.java
+++ src/com/mulgasoft/emacsplus/keys/EmacsPlusKeymapProvider.java
@@ -14,10 +14,23 @@ public class EmacsPlusKeymapProvider implements BundledKeymapProvider {
 
   private static final Logger LOG = Logger.getInstance(EmacsPlusKeymapProvider.class);
 
+  /*
   private final ImmutableList<String> keymapFileNames =
       ImmutableList.of("Keymap_EmacsPlus.xml", "Keymap_EmacsPlusMac.xml", "de/Keymap_EmacsPlus.xml",
           "de/Keymap_EmacsPlusMac.xml", "es/Keymap_EmacsPlus.xml", "es/Keymap_EmacsPlusMac.xml",
           "fr/Keymap_EmacsPlus.xml", "fr/Keymap_EmacsPlusMac.xml");
+
+          Ignore international keymaps for now:
+          2017-04-13 12:46:41,489 [  13493]  ERROR - openapi.keymap.impl.KeymapImpl - Cannot find parent scheme Mac Emacs+ for scheme Mac Emacs+ fr
+          java.lang.Throwable
+	          at com.intellij.openapi.diagnostic.Logger.error(Logger.java:132)
+	          at com.intellij.openapi.keymap.impl.KeymapImpl.readExternal(KeymapImpl.kt:506)
+	          at com.intellij.openapi.keymap.impl.DefaultKeymapImpl.readExternal(DefaultKeymapImpl.kt:39)
+  */
+
+  private final ImmutableList<String> keymapFileNames =
+      ImmutableList.of("Keymap_EmacsPlus.xml", "Keymap_EmacsPlusMac.xml");
+
 
   @NotNull
   @Override
